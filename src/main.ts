13b7b4db5fc63e12e001c70a1413411ec21f758e
Ferenc02 <ferenc.sziraki@medieinstitutet.se>
2025-02-10 18:32:26 +0100

    :sparkles: Update ganache database files, enhance error handling in main script, and add message box component for better user feedback

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { showMessageBox } from "./messageBox";
 import "./style.css";
 
-import { ethers, formatEther, parseEther } from "ethers";
+import { ethers, formatEther } from "ethers";
 
-const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+async function checkProvider() {
+  try {
+    const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+
+    await provider.getBlockNumber();
+
+    return provider;
+  } catch (error: any) {
+    showMessageBox(
+      "error",
+      "Error connecting to provider",
+      "❌ No provider found or connection failed. Please run ganache-cli or any other Ethereum client on 127.0.0.1:8545"
+    );
+
+    return null;
+  }
+}
+
+let provider = (await checkProvider()) || new ethers.JsonRpcProvider();
 
 const getBalance = async (address: string) => {
   const balance = await provider.getBalance(address);
@@ -22,5 +41,5 @@ const address = "0xe092b1fa25DF5786D151246E492Eed3d15EA4dAA";
 getBalance(address).then((balance) => {
   document.querySelector<HTMLDivElement>(
     "#app"
-  )!.innerHTML = `<h1>${balance} ETH</h1>`;
+  )!.innerHTML = `<h1>Balance: ${balance} ETH</h1>`;
 });
---
+++ src/messageBox.ts
@@ -0,0 +1,54 @@
+export const showMessageBox = (
+  type: string,
+  title: string,
+  message: string
+) => {
+  const messageBoxParent = document.createElement("div");
+
+  if (type !== "error" && type !== "success") {
+    showMessageBox("error", "Error", "Invalid message box type");
+    return;
+  }
+
+  messageBoxParent.className =
+    "message-box fixed w-full h-full bg-opacity-75 bg-gray-900/65 flex justify-center items-center  z-50";
+
+  messageBoxParent.innerHTML = `
+        <!-- Message box -->
+    <div
+      class="message-box fixed w-full h-full bg-opacity-75 bg-gray-900/65 flex justify-center items-center  z-50 fade-in "
+    >
+      <div
+        class="message-box__content grow-animation bg-white rounded-lg mx-auto p-4 flex flex-col items-center gap-8 shadow-lg max-w-xs min-w-xs md:max-w-sm min-w-sm"
+      >
+        <span
+          class="message-box__icon material-symbols-outlined text-ganache-yellow-light !text-8xl mt-4"
+        >
+          ${type === "error" ? "cancel" : "check_circle"}
+        </span>
+        <h2
+          class="message-box__title text-center text-2xl font-bold text-gray-800"
+        >
+          ${title}
+        </h2>
+        <p class="message-box__text text-gray-700">
+            ${message}
+        </p>
+        <button class="message-box__button button bg-ganache-yellow-light hover:bg-ganache-yellow-dark transition-colors w-full cursor-pointer">Close</button>
+      </div>
+    </div>
+
+
+    `;
+
+  messageBoxParent.addEventListener("click", (event) => {
+    if (
+      event.target === messageBoxParent.querySelector(".message-box") ||
+      event.target === messageBoxParent.querySelector(".message-box__button")
+    ) {
+      messageBoxParent.remove();
+    }
+  });
+
+  document.body.appendChild(messageBoxParent);
+};
