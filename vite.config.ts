020a1d45aa8e5251a9bafaf4ebbd9522efa4652c
Ferenc02 <ferenc.sziraki@medieinstitutet.se>
2025-02-10 14:47:07 +0100

    :tada: Initialize Vite project with TypeScript, Tailwind CSS, and basic HTML structure; Import ganache

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+export default defineConfig({
+  root: "src",
+
+  plugins: [tailwindcss()],
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        nested: resolve(__dirname, "src/nested/index.html"),
+      },
+    },
+  },
+});
