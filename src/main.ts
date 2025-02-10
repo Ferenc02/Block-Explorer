72f7e352d71905a8f2478f39312df509fa4ea93a
Ferenc02 <ferenc.sziraki@medieinstitutet.se>
2025-02-10 17:53:43 +0100

    :sparkles: Update ganache database files and enhance main script to fetch and display ETH balance as test

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,26 @@
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <h1>Hello Vite!</h1>
-`;
+import { ethers, formatEther, parseEther } from "ethers";
+
+const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+
+const getBalance = async (address: string) => {
+  const balance = await provider.getBalance(address);
+
+  let balanceInEther = formatEther(balance);
+
+  balanceInEther = parseFloat(balanceInEther).toFixed(2);
+  return balanceInEther;
+};
+
+document.querySelector<HTMLDivElement>(
+  "#app"
+)!.innerHTML = `<h1>Loading...</h1>`;
+
+const address = "0xe092b1fa25DF5786D151246E492Eed3d15EA4dAA";
+
+getBalance(address).then((balance) => {
+  document.querySelector<HTMLDivElement>(
+    "#app"
+  )!.innerHTML = `<h1>${balance} ETH</h1>`;
+});
