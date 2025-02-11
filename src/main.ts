d4c8d6c3fbee4ac7e39f9c5ef78b79680a360c85
Ferenc02 <ferenc.sziraki@medieinstitutet.se>
2025-02-11 16:40:00 +0100

    :sparkles: Update ganache database files, enhance logging, and implement light/dark mode styles in the application

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,51 @@
-import { showMessageBox } from "./messageBox";
+// import { showMessageBox } from "./messageBox";
 import "./style.css";
 
-import { ethers, formatEther } from "ethers";
+// import { ethers, formatEther } from "ethers";
 
-async function checkProvider() {
-  try {
-    const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
-
-    await provider.getBlockNumber();
-
-    return provider;
-  } catch (error: any) {
-    showMessageBox(
-      "error",
-      "Error connecting to provider",
-      "❌ No provider found or connection failed. Please run ganache-cli or any other Ethereum client on 127.0.0.1:8545"
-    );
-
-    return null;
-  }
-}
-
-let provider = (await checkProvider()) || new ethers.JsonRpcProvider();
-
-const getBalance = async (address: string) => {
-  const balance = await provider.getBalance(address);
-
-  let balanceInEther = formatEther(balance);
-
-  balanceInEther = parseFloat(balanceInEther).toFixed(2);
-  return balanceInEther;
+const init = async () => {
+  // alert("Hello World");
 };
 
-document.querySelector<HTMLDivElement>(
-  "#app"
-)!.innerHTML = `<h1>Loading...</h1>`;
+// async function checkProvider() {
+//   try {
+//     const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
 
-const address = "0xe092b1fa25DF5786D151246E492Eed3d15EA4dAA";
+//     await provider.getBlockNumber();
 
-getBalance(address).then((balance) => {
-  document.querySelector<HTMLDivElement>(
-    "#app"
-  )!.innerHTML = `<h1>Balance: ${balance} ETH</h1>`;
-});
+//     return provider;
+//   } catch (error: any) {
+//     showMessageBox(
+//       "error",
+//       "Error connecting to provider",
+//       "❌ No provider found or connection failed. Please run ganache-cli or any other Ethereum client on 127.0.0.1:8545"
+//     );
+
+//     return null;
+//   }
+// }
+
+// let provider = (await checkProvider()) || new ethers.JsonRpcProvider();
+
+// const getBalance = async (address: string) => {
+//   const balance = await provider.getBalance(address);
+
+//   let balanceInEther = formatEther(balance);
+
+//   balanceInEther = parseFloat(balanceInEther).toFixed(2);
+//   return balanceInEther;
+// };
+
+// document.querySelector<HTMLDivElement>(
+//   "#app"
+// )!.innerHTML = `<h1>Loading...</h1>`;
+
+// const address = "0xe092b1fa25DF5786D151246E492Eed3d15EA4dAA";
+
+// getBalance(address).then((balance) => {
+//   document.querySelector<HTMLDivElement>(
+//     "#app"
+//   )!.innerHTML = `<h1>Balance: ${balance} ETH</h1>`;
+// });
+
+init();
