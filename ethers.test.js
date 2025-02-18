28f2d27bed2a2e99b7ab9616a84cca8378aa0a11
Ferenc02 <ferenc.sziraki@medieinstitutet.se>
2025-02-18 15:34:16 +0100

    :sparkles: Add tests for transaction creation and enhance existing provider tests in ethers.test.js

--- ethers.test.js
+++ ethers.test.js
@@ -1,3 +1,4 @@
+// --- Other Imports ---
 import { describe, beforeEach, it, expect } from "vitest";
 import {
   checkProvider,
@@ -6,8 +7,14 @@ import {
   initializeProvider,
   getTotalWallets,
   getNetworkName,
+  sendTransaction,
 } from "/src/scripts/ethers";
 
+/**
+ * Test Suite for Ethers.js
+ * It uses the real provider to test the functions so it will only work with a real provider
+ * In a real environment, the provider should be mocked to avoid unnecessary costs and stress on the network
+ */
 describe("Provider Testing", async () => {
   it("Should return true if the provider is loaded ", async () => {
     let providerExists = await checkProvider();
@@ -55,5 +62,40 @@ describe("Retrieve Wallet Address", () => {
     const walletAddress = await provider.listAccounts();
     console.log("Wallet Address: ", walletAddress);
     expect(walletAddress).toBeTruthy();
+  });
+});
+
+describe("Create a transaction", () => {
+  it("Should create a transaction", async () => {
+    await initializeProvider();
+    const walletAddress = await provider.listAccounts();
+
+    const from = walletAddress[0].address;
+    const to = walletAddress[1].address;
+    const amountToSend = 0.1;
+
+    const transactionTemplate = {
+      accessList: expect.any(Array),
+      blobVersionedHashes: null,
+      blockHash: expect.any(String),
+      blockNumber: expect.any(Number),
+      chainId: expect.any(BigInt),
+      data: "0x",
+      from: expect.any(String),
+      gasLimit: expect.any(BigInt),
+      gasPrice: expect.any(BigInt),
+      hash: expect.any(String),
+      maxFeePerBlobGas: null,
+      maxFeePerGas: expect.any(BigInt),
+      maxPriorityFeePerGas: expect.any(BigInt),
+      nonce: expect.any(Number),
+      to: expect.any(String),
+      type: 2,
+      value: expect.any(BigInt),
+    };
+
+    const tx = await sendTransaction(from, to, amountToSend.toString());
+    console.log("Transaction: ", tx);
+    expect(tx).toMatchObject(transactionTemplate);
   });
 });
