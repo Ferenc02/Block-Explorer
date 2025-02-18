5c623eb20870c7a4a502b6942a81e0964cbe6e3e
Ferenc02 <ferenc.sziraki@medieinstitutet.se>
2025-02-18 14:43:30 +0100

    :sparkles: Remove  Ganache database files and update server command in package.json; Also fixed messagebox styling for dark mode

--- src/scripts/messageBox.ts
+++ src/scripts/messageBox.ts
@@ -41,7 +41,7 @@ export const showMessageBox = (
       class="message-box fixed w-full h-full bg-opacity-75 bg-gray-900/65 flex justify-center items-center  z-50 fade-in scale-100"
     >
       <div
-        class="message-box__content grow-animation bg-white rounded-lg mx-auto p-4 flex flex-col items-center gap-8 shadow-lg max-w-[90vw] max-h-[80vh]  md:max-w-md md:min-w-sm"
+        class="message-box__content grow-animation bg-search rounded-lg mx-auto p-4 flex flex-col items-center gap-8 shadow-lg max-w-[90vw] max-h-[80vh]  md:max-w-md md:min-w-sm"
       >
         <span
           class="message-box__icon material-symbols-outlined text-ganache-yellow-light !text-8xl mt-4"
@@ -49,7 +49,7 @@ export const showMessageBox = (
           ${type === "error" ? "cancel" : "check_circle"}
         </span>
         <h2
-          class="message-box__title text-center text-2xl font-bold text-gray-800"
+          class="message-box__title text-center text-2xl font-bold text-inherit"
         >
           ${title}
         </h2>
